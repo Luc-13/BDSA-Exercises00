1d442ba11ce20983fa6f99dd768937457e4b9889
LF <80175467+Luc-13@users.noreply.github.com>
2022-09-08 11:11:07 +0200

    implemented exceptions and error handling (#5)
    Co-authored-by: lufr@itu.dk <lufr@itu.dk>

--- HelloWorld.Tests/leapYearTest.cs
+++ HelloWorld.Tests/leapYearTest.cs
@@ -23,6 +23,9 @@ public class leapYearTest
         var leap = new leapYear();
 
         var result = leap.UserInput();
+
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
         
         var stringReader = new StringReader("1600");
         Console.SetIn(stringReader);
@@ -35,10 +38,30 @@ public class leapYearTest
 
         var result = leap.UserInput();
 
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+        
         var stringReader = new StringReader("1900");
         Console.SetIn(stringReader);
 
         result.Should().Be("nay");
     }
     */
+     [Fact]
+    public void is_divisible_by_four_unless_also_by_100_unless_divisible_by_400()
+    {
+        var leap = new leapYear();
+
+        var result = leap.IsLeapYear(1600);
+        result.Should().Be(true);
+    }
+    [Fact]
+    public void above_or_below_1582(){
+        var leap = new leapYear();
+
+        var on = leap.IsLeapYear(1582);
+        Action under = () => leap.IsLeapYear(1500);
+        on.Should().Be(false);
+        under.Should().Throw<ArgumentException>();
+    }
 }
